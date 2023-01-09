9594afc439d522e9ca0c24a100912146b79083fb
priyankachachar <pchikane47@gmail.com>
2023-01-10 00:49:04 +0530

    LinkedHashSetDemoAdded

---
+++ CollectionsAgain/src/com/velocity/LinkedHashSetDemo.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedHashSetDemo {
+public static void main(String[] args) {
+	Set<String> set =new LinkedHashSet<String>();
+	set.add("Dhruv");
+	set.add("Priyanka");
+	set.add("Akash");
+	System.out.println(set);
+}
+}
